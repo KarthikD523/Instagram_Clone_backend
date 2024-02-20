bb41e8a7874a7173cfaed5e9d655fd16f0d26ac9
KarthikD523 <92270403+KarthikD523@users.noreply.github.com>
2024-02-20 14:29:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {MONGOURI}=require(`./keys`)
 const app=express()
 
 app.use(cors({
-    origin: 'https://instagram-clone-frontend-tan.vercel.app/',
+    origin: 'https://instagram-clone-frontend-tan.vercel.app/SignIn',
     methods: ["POST","PUT","GET"],
     credentials: true
 }));
